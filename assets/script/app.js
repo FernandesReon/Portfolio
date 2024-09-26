271bdc22e2882d1e2df2d6433385374c5bd3edc3
Reon Fernandes <reondias@gmail.com>
2024-09-26 22:15:50 +0530

    service section
    service section is ready.

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,3 +3,29 @@ window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+//Services section - Modal
+// Select all the modals and buttons
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+// Function to show the modal
+var modal = function(modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+// Attach event listener to each "Learn More" button
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+// Close modal when clicking the close button
+modalCloseBtns.forEach((modalCloseBtn, i) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals[i].classList.remove("active");
+    });
+});
