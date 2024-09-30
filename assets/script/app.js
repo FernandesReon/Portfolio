9fd273dd6919e0bd92cade16bf497eeca649dcb6
Reon Fernandes <reondias@gmail.com>
2024-09-30 22:00:12 +0530

    website almost ready
    all set only responsive behaviour left.

--- assets/script/app.js
+++ assets/script/app.js
@@ -45,3 +45,16 @@ portfolioCloseBtns.forEach((portfolioCloseBtn, i) => {
         portfolioModals[i].classList.remove("active");
     });
 });
+
+
+// Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -36,3 +36,25 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+
+// Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
